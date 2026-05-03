8f94c5b82135d92ac56e773a35e23a98ebd56db9
Emanuel G. Sousa <emanuelgoncalves434@gmail.com>
2026-05-03 18:07:19 -0300

    feat(views): default dark mode implement, correct SVGs colors (reestructure svg and invert it).

--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -1,29 +1,32 @@
 const btnTheme = document.querySelector('.btn-theme');
 const html = document.documentElement;
-const icon = document.querySelector('.icon');
-
+const icon = btnTheme.querySelector('div'); 
 
 function initTheme() {
     const savedTheme = localStorage.getItem('theme');
-    
+
     if (savedTheme) {
         html.setAttribute('data-theme', savedTheme);
-    } else {
-        html.setAttribute('data-theme', 'light');
-    }
+    } // nao necessita do else pois o html ja esta com data-theme="dark"
 }
-function toggleTheme(){
-    const currentTheme = html.getAttribute('data-theme');
-    if (currentTheme === 'dark'){
-        html.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme','light');
-        icon.style.backgroundImage = "url(imgs/sun.svg)";
-    } else {
-        html.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme','dark');
-        icon.style.backgroundImage = "url(imgs/moon.svg)";
-    }
-}
-initTheme();
 
+function toggleTheme() {
+    const currentTheme = html.getAttribute('data-theme');
+    
+    btnTheme.classList.add('animating');
+
+    setTimeout(() => {
+        if (currentTheme === 'dark') {
+            html.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            html.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+        
+        btnTheme.classList.remove('animating');
+    }, 150);
+}
+
+initTheme();
 btnTheme.addEventListener('click', toggleTheme);
