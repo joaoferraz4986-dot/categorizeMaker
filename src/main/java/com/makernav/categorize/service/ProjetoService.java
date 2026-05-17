b1f76fdfbfdf24732f0f16058ebe463e93793227
Yagoria-1 <yagoalexandrealves@gmail.com>
2026-05-17 17:16:03 -0300

    Update Controller e Service na parte lógica

--- src/main/java/com/makernav/categorize/service/ProjetoService.java
+++ src/main/java/com/makernav/categorize/service/ProjetoService.java
@@ -1,8 +1,9 @@
 package com.makernav.categorize.service;
 
+import com.makernav.categorize.dto.ProjetoRequestDTO;
+import com.makernav.categorize.dto.ProjetoResponseDTO;
 import com.makernav.categorize.infra.repository.ProjetoRepository;
 import com.makernav.categorize.model.Projeto;
-import com.makernav.categorize.infra.repository.ProjetoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +16,28 @@ public class ProjetoService {
     @Autowired
     private ProjetoRepository projetoRepository;
 
-    public List<Projeto> listarTodos() {
-        return projetoRepository.findAll();
+    public List<ProjetoResponseDTO> listarTodos() {
+        return projetoRepository.findAll()
+                .stream()
+                .map(p -> new ProjetoResponseDTO(p.getNome(), p.getCategoria(), p.getDescricao()))
+                .toList();
     }
 
-    public Optional<Projeto> buscarPorId(int id) {
-        return projetoRepository.findById(id);
-    }
 
-    public Projeto salvar(Projeto projeto) {
-        return projetoRepository.save(projeto);
+    public Optional<ProjetoResponseDTO> buscarPorId(int id) {
+        return projetoRepository.findById(id)
+                .map(p -> new ProjetoResponseDTO(p.getNome(), p.getCategoria(), p.getDescricao()));
+    }
+    public ProjetoResponseDTO salvar(ProjetoRequestDTO dto) {
+
+        Projeto projeto = new Projeto();
+        projeto.setNome(dto.nome());
+        projeto.setCategoria(dto.categoria());
+        projeto.setDescricao(dto.descricao());
+
+        Projeto salvo = projetoRepository.save(projeto);
+
+        return new ProjetoResponseDTO(salvo.getNome(), salvo.getCategoria(), salvo.getDescricao());
     }
 
     public void deletar(int id) {
