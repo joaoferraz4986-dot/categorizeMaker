a3ed7f9feb5eeade3f490b47dea5003661680214
Yagoria-1 <yagoalexandrealves@gmail.com>
2026-05-17 16:41:59 -0300

    Add de Request,Response DTO e Categoria de Projeto

---
+++ src/main/java/com/makernav/categorize/dto/ProjetoRequestDTO.java
@@ -0,0 +1,11 @@
+package com.makernav.categorize.dto;
+
+import com.makernav.categorize.model.CategoriaProjeto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProjetoRequestDTO(
+    @NotBlank String nome,
+    @NotNull CategoriaProjeto categoria,
+    @NotBlank String descrição
+) {}
---
+++ src/main/java/com/makernav/categorize/model/CategoriaProjeto.java
@@ -0,0 +1,8 @@
+package com.makernav.categorize.model;
+
+public enum CategoriaProjeto {
+    WEB,
+    MOBILE,
+    DESKTOP,
+    EMBARCADOS
+}
